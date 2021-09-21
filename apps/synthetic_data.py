1a627491309268e264cdf9510594abd14b671702
Adrien Treuille <adrien.g.treuille@gmail.com>
2021-09-21 07:28:25 -0700

    Starting to alllow the user to create a new table.

--- apps/synthetic_data.py
+++ apps/synthetic_data.py
@@ -1,37 +1,40 @@
-from numpy import number
+# from numpy import number
+import sqlalchemy as sa
 import streamlit as st
-from streamlit import caching
+
+# from streamlit import caching
 import pandas as pd
 import snowflake.connector
 from snowflake.connector.connection import SnowflakeConnection
+from snowflake.connector.pandas_tools import pd_writer
 import yaml
 from typing import List, Tuple
 import random
 
-INFORMATION_SCHEMA_TABLES_COLUMNS = [
-    "TABLE_CATALOG",
-    "TABLE_SCHEMA",
-    "TABLE_NAME",
-    "TABLE_OWNER",
-    "TABLE_TYPE",
-    "IS_TRANSIENT",
-    "CLUSTERING_KEY",
-    "ROW_COUNT",
-    "BYTES",
-    "RETENTION_TIME",
-    "SELF_REFERENCING_COLUMN_NAME",
-    "REFERENCE_GENERATION",
-    "USER_DEFINED_TYPE_CATALOG",
-    "USER_DEFINED_TYPE_SCHEMA",
-    "USER_DEFINED_TYPE_NAME",
-    "IS_INSERTABLE_INTO",
-    "IS_TYPED",
-    "COMMIT_ACTION",
-    "CREATED",
-    "LAST_ALTERED",
-    "AUTO_CLUSTERING_ON",
-    "COMMENT",
-]
+# INFORMATION_SCHEMA_TABLES_COLUMNS = [
+#     "TABLE_CATALOG",
+#     "TABLE_SCHEMA",
+#     "TABLE_NAME",
+#     "TABLE_OWNER",
+#     "TABLE_TYPE",
+#     "IS_TRANSIENT",
+#     "CLUSTERING_KEY",
+#     "ROW_COUNT",
+#     "BYTES",
+#     "RETENTION_TIME",
+#     "SELF_REFERENCING_COLUMN_NAME",
+#     "REFERENCE_GENERATION",
+#     "USER_DEFINED_TYPE_CATALOG",
+#     "USER_DEFINED_TYPE_SCHEMA",
+#     "USER_DEFINED_TYPE_NAME",
+#     "IS_INSERTABLE_INTO",
+#     "IS_TYPED",
+#     "COMMIT_ACTION",
+#     "CREATED",
+#     "LAST_ALTERED",
+#     "AUTO_CLUSTERING_ON",
+#     "COMMENT",
+# ]
 
 
 SAMPLE_DATABASE = "SAMPLE_EMAILS"
@@ -60,18 +63,18 @@ def run_query(query: str):
 
 
 # @st.cache(ttl=600)
-def get_databases() -> list:
+def get_databases() -> List[str]:
     return [row[1] for row in run_query("SHOW DATABASES;")]
 
 
 # @st.cache(ttl=600)
-def get_tables(database) -> pd.DataFrame:
-    tables = pd.DataFrame(
-        run_query(f"SELECT * FROM {database}.INFORMATION_SCHEMA.TABLES LIMIT 10;")
-    )
+# def get_tables(database) -> pd.DataFrame:
+#     tables = pd.DataFrame(
+#         run_query(f"SELECT * FROM {database}.INFORMATION_SCHEMA.TABLES LIMIT 10;")
+#     )
 
-    tables.columns = INFORMATION_SCHEMA_TABLES_COLUMNS
-    return tables
+#     tables.columns = INFORMATION_SCHEMA_TABLES_COLUMNS
+#     return tables
 
 
 @_snowflake_cache()
@@ -92,6 +95,94 @@ def randomize_names(key: str):
     names = load_names()
     random_names = random.sample(names[key], 10)
     setattr(st.session_state, key, random_names)
+
+
+def create_table(table_name: str, df: pd.DataFrame):
+    """Called when the user creates a new table of email addresses."""
+    st.warning(f"Creating `{SAMPLE_DATABASE}.{table_name}` with len `{len(df)}`.")
+    st.text(pd_writer)
+    # st.write(sa.create_engine)
+    from snowflake.sqlalchemy import URL
+    from sqlalchemy import create_engine
+
+    engine = sa.create_engine(**st.secrets["snowflake"])
+    st.text(type(url))
+    st.text(url)
+    conn2 = engine.connect()
+    try:
+        # conn2.execute(<SQL>)
+        df.to_sql(
+            f"{SAMPLE_DATABASE}.{table_name}",
+            conn,
+            if_exists="replace",
+            index=False,
+            method=pd_writer,
+        )
+        st.success(f"Created `{table_name}` with len `{len(df)}`.")
+    finally:
+        conn2.close()
+        engine.dispose()
+    # account = 'myorganization-myaccount',
+    # user = 'testuser1',
+    # password = '0123456',
+    # database = 'testdb',
+    # schema = 'public',
+    # warehouse = 'testwh',
+    # role='myrole',
+    # ))
+
+
+
+# pandas.core.generic.to_sql(name: 'str', con, schema=None, if_exists: 'str' = 'fail', index: 'bool_t' = True, index_label=None, chunksize=None, dtype: 'DtypeArg | None' = None, method=None) -> 'None'
+# Write records stored in a DataFrame to a SQL database.
+
+# Databases supported by SQLAlchemy [1]_ are supported. Tables can be
+# newly created, appended to, or overwritten.
+
+# Parameters
+# ----------
+# name : str
+#     Name of SQL table.
+# con : sqlalchemy.engine.(Engine or Connection) or sqlite3.Connection
+#     Using SQLAlchemy makes it possible to use any DB supported by that
+#     library. Legacy support is provided for sqlite3.Connection objects. The user
+#     is responsible for engine disposal and connection closure for the SQLAlchemy
+#     connectable See `here                 <https://docs.sqlalchemy.org/en/13/core/connections.html>`_.
+
+# schema : str, optional
+#     Specify the schema (if database flavor supports this). If None, use
+#     default schema.
+# if_exists : {'fail', 'replace', 'append'}, default 'fail'
+#     How to behave if the table already exists.
+
+#     * fail: Raise a ValueError.
+#     * replace: Drop the table before inserting new values.
+#     * append: Insert new values to the existing table.
+
+# index : bool, default True
+#     Write DataFrame index as a column. Uses `index_label` as the column
+#     name in the table.
+# index_label : str or sequence, default None
+#     Column label for index column(s). If None is given (default) and
+#     `index` is True, then the index names are used.
+#     A sequence should be given if the DataFrame uses MultiIndex.
+# chunksize : int, optional
+#     Specify the number of rows in each batch to be written at a time.
+#     By default, all rows will be written at once.
+# dtype : dict or scalar, optional
+#     Specifying the datatype for columns. If a dictionary is used, the
+#     keys should be the column names and the values should be the
+#     SQLAlchemy types or strings for the sqlite3 legacy mode. If a
+#     scalar is provided, it will be applied to all columns.
+# method : {None, 'multi', callable}, optional
+#     Controls the SQL insertion clause used:
+
+#     * None : Uses standard SQL ``INSERT`` clause (one per row).
+#     * 'multi': Pass multiple values in a single ``INSERT`` clause.
+#     * callable with signature ``(pd_table, conn, keys, data_iter)``.
+
+#     Details and a sample callable implementation can be found in the
+#     section :ref:`insert method <io.sql.method>`.
 
 
 def create_synthetic_data(conn: SnowflakeConnection):
@@ -115,8 +206,9 @@ def create_synthetic_data(conn: SnowflakeConnection):
     # st.write("tables", tables)
 
     st.write("## Create synthetic data")
-    names = load_names()
 
+    # Show select boxes for the first and last names.
+    names = load_names()
     name_types = [("first name", "firstnames"), ("last name", "lastnames")]
     for name_type, key in name_types:
         if key not in st.session_state:
@@ -145,12 +237,19 @@ def create_synthetic_data(conn: SnowflakeConnection):
         )
     )
     st.write(df)
-    st.write(f"n_rows: `{n_rows}`")
-    st.write(f"n_firstnames: `{n_firstnames}`")
-    st.write(f"n_lastnames: `{n_lastnames}`")
-    # st.write(f"num_rows: `{n_rows}`")
-    # st.write(f"num_rows: `{n_rows}`")
-    # st.write(f"num_rows: `{n_rows}`")
+    table_name = st.text_input("Table name").strip()
+    if not table_name:
+        st.warning("Enter a table name above an press enter to create a new table.")
+    else:
+        st.button(
+            f'Create "{table_name}"', on_click=create_table, args=(table_name, df)
+        )
+    # st.write(st.text_input)
+    # st.write(df.to_sql)
+
+    # st.write(f"n_rows: `{n_rows}`")
+    # st.write(f"n_firstnames: `{n_firstnames}`")
+    # st.write(f"n_lastnames: `{n_lastnames}`")
 
     # table_name = st.text_input("Table name")
     # if st.button("Insert a table"):
