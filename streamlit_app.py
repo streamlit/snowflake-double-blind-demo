2aa4eaed63666d89d07e1e83faa238a5d7e56487
Adrien Treuille <adrien.g.treuille@gmail.com>
2021-11-08 20:04:56 -0800

    Renamed contact tables

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,24 +163,6 @@ def synthetic_data_page():
     )
 
 
-def advanced_options_page():
-    """advanced form, destroy database"""
-    st.warning("These have not been properly debugged.")
-    return
-    if st.button(f"DROP PUBLIC Tables"):
-        st.warning(f"Dropping all tables in schema `PUBLIC`")
-        engine = get_engine(st.secrets["snowflake"])
-        for table in tables:
-            run_query(f"DROP TABLE STREAMLIT_DEMO_DB.PUBLIC.{table}", engine)
-        st.button("Reload this page")
-
-    if st.button(f"Nuke STREAMLIT_DEMO_DB"):
-        st.warning(f"Destroying `STREAMLIT_DEMO_DB...`")
-        run_query(f"DROP DATABASE STREAMLIT_DEMO_DB CASCADE")
-        st.success(f"Destroyed `STREAMLIT_DEMO_DB`")
-        st.button("Reload this page")
-
-
 def intro_page():
     """Show the text the user first sees when they run the app."""
     with open("README.md") as readme:
@@ -240,7 +222,7 @@ def main():
 
     # Show the tables
     st.sidebar.write("---")
-    st.sidebar.subheader("Tables of Contacts")
+    st.sidebar.subheader("Contacts Tables")
     st.sidebar.table(tables)
 
     # Run the selected mode
